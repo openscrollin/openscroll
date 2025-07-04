7c3eec6850538478ea37d0d371010d6339469859
Rupam Rahate <rupamr@Rupams-MacBook-Air.local>
2025-07-05 00:59:45 +0530

    ✅ Updated Articles.js, fixed onClick, CORS, and styles

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,13 +1,12 @@
 const admin = require('firebase-admin');
 const { getStorage } = require('firebase-admin/storage');
-
-const serviceAccount = require('./firebaseServiceAccountKey.json'); // download from Firebase console
+const path = require('path');
+const serviceAccount = require(path.resolve(__dirname, 'firebaseServiceAccountKey.json'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'openscroll-auth.firebasestorage.app', // replace with your bucket name
+  storageBucket: 'openscroll-auth.firebasestorage.app', // Replace with your bucket name
 });
 
 const bucket = getStorage().bucket();
-
 module.exports = bucket;
